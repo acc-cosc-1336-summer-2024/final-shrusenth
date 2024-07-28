d9bae06ff2480fd4b32896338a2b526783e63835
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-07-27 22:56:41 -0500

    Update main.py

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,9 @@
-#add import
+from question_a import Stock
+
+stock = Stock("CAT", "Caterpillar Inc.")
+
+company_name = stock.get_company_name()
+symbol = stock.get_symbol()
+
+print(symbol)
+print(company_name)
