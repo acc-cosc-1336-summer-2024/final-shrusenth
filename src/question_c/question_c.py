1b85ec924716412655aa631ec03448fe8b03088b
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-07-27 20:13:30 -0500

    Update question_c.py

--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,23 @@
-#write functions here, don't add input('') statements here!
+def create_multiplication_table():
+    table = []
+    for i in range(1, 6):
+        row = []
+        for j in range(1, 11):
+            row.append(i * j)
+        table.append(row)
+    return table
+
+def display_multiplication_table(table):
+    for row in table:
+        print(' '.join(map(str, row)))
+
+def user_menu():
+    while True:
+        choice = input("Would you like to create a new table? (type 'yes' or no')").strip().lower()
+        if choice == 'yes':
+            table = create_multiplication_table()
+            display_multiplication_table(table)
+        elif choice == 'no':
+            break
+        else:
+            print("Invalid input.")
